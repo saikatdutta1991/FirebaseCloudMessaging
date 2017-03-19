6d66cc1048b93acd6f62eb85dab1cc1906987627
Saikat <saikatdutta1991@gmail.com>
2017-03-19 12:00:58 +0530

    composer updated and post script event handler added

---
+++ src/PostInstallEventHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Saikat\FirebaseCloudMessaging;
+
+use ComposerScriptEvent;
+
+
+class PostInstallEventHandler
+{
+	public static function copyConfig()
+	{
+		echo "called";
+	}
+}
